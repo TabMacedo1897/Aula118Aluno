ece33e03d3105b52879a5d2557318f77050dae97
m1gu3l022 <tabata.macedo1897@gmail.com>
2023-11-20 10:21:46 -0300

    atualização para a atividade do aluno

--- app.py
+++ app.py
@@ -13,24 +13,8 @@ def home():
     entries = show_entry()
     return render_template("index.html", entries=entries)
     
-# Preveja a emoção
-@app.route("/predict-emotion", methods=["POST"])
-def predict_emotion():
-    input_text = request.json.get("text")
-    if not input_text:
-        return jsonify({
-            "status": "error",
-            "message": "Digite um texto para prever a emoção!"
-        }), 400
-    else:
-        predicted_emotion, predicted_emotion_img_url = predict(input_text)                         
-        return jsonify({
-            "data": {
-                "predicted_emotion": predicted_emotion,
-                "predicted_emotion_img_url": predicted_emotion_img_url
-            },
-            "status": "success"
-        }), 200
+# Configure a função: Preveja a emoção
+
 
 # Salve a entrada
 @app.route("/save-entry", methods=["POST"])
